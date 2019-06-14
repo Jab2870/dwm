902b0c87a8a4fd9cccee07c5c093ec4a2d2dc678
Jonathan Hodgson <git@jonathanh.co.uk>
2019-06-14 13:51:45 +0100

    Makes thundirbird always open on tag 9

--- config.def.h
+++ config.def.h
@@ -29,10 +29,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   isterminal noswallow monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           0,         0,        -1 },
-	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,         0,        -1 },
-	{ "xterm-256color",       NULL,       NULL,       0,            0,           1,         1,        -1 },
+	/* class                  instance    title       tags mask     isfloating   isterminal noswallow    monitor */
+	//{ "Gimp",               NULL,       NULL,       0,            0,           0,         0,           -1 },
+	//{ "Firefox",            NULL,       NULL,       1 << 8,       0,           0,         0,           -1 },
+	{ "xterm-256color",       NULL,       NULL,       0,            0,           1,         1,           -1 },
+	{ "Thunderbird",          NULL,       NULL,       1 << 8 ,      0,           0,         0,           -1 },
 };
 
 /* layout(s) */
